ba4d043b6e5e4fcea2a7267393a738bcc862655c
nqn.duy@gmail.com <nqn.duy@gmail.com>
2022-07-23 18:09:26 +0700

    done homepage, product detail

--- dest/main.js
+++ dest/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             desktop: 1199
         };
 
-    // DETECT DEVICE 
+    // DETECT DEVICE
     function mobileDetect() {
         let md = new MobileDetect(window.navigator.userAgent);
         if (md.mobile() != null || md.tablet() != null) {
@@ -45,3 +45,51 @@ $(document).ready(function () {
     init();
 
 })
+
+let $carousel = $(".product__list");
+$carousel.flickity({
+	cellAlign: "left",
+	contain: true,
+	wrapAround: true,
+	prevNextButtons: false,
+	pageDots: false,
+});
+$(".product .product__control .product__control-btn.prev").on("click", function () {
+	$carousel.flickity("previous");
+});
+$(".product .product__control .product__control-btn.next").on("click", function () {
+	$carousel.flickity("next");
+});
+
+let $sliderDetailProduct = $(".productDetail .productDetail-content .slider");
+$sliderDetailProduct.flickity({
+	contain: true,
+	prevNextButtons: false,
+	pageDots: false,
+	fullscreen: true,
+});
+
+let $thumbDetailProduct = $(".productDetail .productDetail-content .thumb .thumb__list");
+let $thumbPrev = $(".productDetail .productDetail-content .thumb .thumb__control-btn.prev");
+let $thumbNext = $(".productDetail .productDetail-content .thumb .thumb__control-btn.next");
+
+asNavFor: $(".productDetail .productDetail-content .slider")[0];
+
+$thumbDetailProduct.flickity({
+    asNavFor: ".productDetail .productDetail-content .slider",
+	cellAlign: "left",
+	wrapAround: false,
+	draggable: false,
+	contain: true,
+	prevNextButtons: false,
+	pageDots: false,
+	lazyLoad: 5,
+});
+$thumbPrev.on("click", function () {
+    $sliderDetailProduct.flickity("previous");
+	$thumbDetailProduct.flickity("previous");
+});
+$thumbNext.on("click", function () {
+    $sliderDetailProduct.flickity("next");
+	$thumbDetailProduct.flickity("next");
+});
